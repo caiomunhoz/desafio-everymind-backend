3ab713d4724296ae5b5cd3c1e87bb604d6516472
caiomunhoz <caio.gmunhoz@gmail.com>
2024-01-24 14:30:06 -0300

    ProductService CRUD implemented

--- src/main/java/com/everymind/nunesSports/services/ProductService.java
+++ src/main/java/com/everymind/nunesSports/services/ProductService.java
@@ -4,23 +4,60 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.everymind.nunesSports.entities.Product;
 import com.everymind.nunesSports.repositories.ProductRepository;
+import com.everymind.nunesSports.services.exceptions.DatabaseException;
+import com.everymind.nunesSports.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
-	public List<Product> findAll(){
+
+	public Product insert(Product product) {
+		return repository.save(product);
+	}
+
+	public List<Product> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Product findById(Long id) {
 		Optional<Product> product = repository.findById(id);
-		return product.get();
+		return product.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	public Product update(Long id, Product product) {
+		try {
+			Product entity = repository.getReferenceById(id);
+			updateData(entity, product);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Product entity, Product product) {
+		entity.setName(product.getName());
+		entity.setPrice(product.getPrice());
+		entity.setDescription(product.getDescription());
+	}
+
+	public void delete(Long id) {
+		if (repository.existsById(id)) {
+			try {
+				repository.deleteById(id);
+			} catch (DataIntegrityViolationException e) {
+				throw new DatabaseException(e.getMessage());
+			}
+		} else {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 }
